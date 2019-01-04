bd6d6979e2b209725508cc5fdad84ab3297fe0c2
willianjbl <37624709+willianjbl@users.noreply.github.com>
2019-01-04 10:23:30 -0200

    Setting up the buttons

--- js/controller/calcController.js
+++ js/controller/calcController.js
@@ -5,8 +5,89 @@ class calcController{
     }
 
     inti(){
-
+        this.initButtonEvents();
     }
+
+    //SETTING UP THE CLICKABLE BUTTONS
+    initButtonEvents(){
+        let buttons = document.querySelectorAll('.btn');
+
+        buttons.forEach(btn=>{
+            this.addEventListenerAll(btn, 'click drag', e=>{
+                let btnText = btn.id;
+                this.execBtn(btnText);
+            });
+        });
+    }
+
+    addEventListenerAll(el, ev, fn){
+        ev.split(" ").forEach(event=>{
+            el.addEventListener(event, fn);
+        });
+    }
+
+    execBtn(value){
+        switch (value) {
+            case "c":
+                
+                break;
+            case "ce":
+                
+                break;
+            case "backspace":
+                
+                break;
+            case "negativoPositivo":
+                
+                break;
+            case "porcento":
+                
+                break;
+            case "raiz":
+                
+                break;
+            case "quadrado":
+                
+                break;
+            case "inverso":
+                
+                break;
+            case "divisao":
+                
+                break;
+            case "multiplicacao":
+                
+                break;
+            case "subtracao":
+                
+                break;
+            case "soma":
+                
+                break;
+            case "ponto":
+                
+                break;
+            case "igual":
+                
+                break;
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+            case "0":
+                
+                break;
+        
+            default:
+                break;
+        }
+    }
+    //END OF BUTTONS
 
     //GETS AND SETTERS
     get displayCalc(){
