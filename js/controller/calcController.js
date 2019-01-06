d97742a4d0b354f0cf112f1098caeb5c8ddb9335
willianjbl <37624709+willianjbl@users.noreply.github.com>
2019-01-06 19:41:53 -0200

    Add basic operations and displays numbers on display

--- js/controller/calcController.js
+++ js/controller/calcController.js
@@ -1,11 +1,17 @@
 class calcController{
     constructor(){
+        this._operation = [];
         this._displayCalc = document.querySelector('#display');
         this.inti();
     }
 
     inti(){
         this.initButtonEvents();
+    }
+
+    //DISPLAYS ERROR ON SCREEN
+    setError(){
+        this.displayCalc = "ERROR";
     }
 
     //SETTING UP THE CLICKABLE BUTTONS
@@ -53,16 +59,16 @@ class calcController{
                 
                 break;
             case "divisao":
-                
+                this.addOperation("/");
                 break;
             case "multiplicacao":
-                
+                this.addOperation("*");
                 break;
             case "subtracao":
-                
+                this.addOperation("-");
                 break;
             case "soma":
-                
+                this.addOperation("+");
                 break;
             case "ponto":
                 
@@ -80,14 +86,85 @@ class calcController{
             case "8":
             case "9":
             case "0":
-                
+                this.addOperation(parseInt(value));
                 break;
         
             default:
+                this.setError();
                 break;
         }
     }
     //END OF BUTTONS
+
+    //VALIDATES IF LAST VALUE WAS AN OPERATOR
+    isOperator(value){
+        return (["+","-","*","/","%"].indexOf(value) > -1);
+    }
+
+    //PUSHES VALUE INTO OPERATION
+    pushOperation(value){
+        this._operation.push(value);
+
+        if(this._operation.length > 3){
+            this.calc();
+        }
+    }
+
+    //SETS A VALUE TO THE LAST OPERATION ITEM
+    setLastOperation(value){
+        this._operation[this._operation.length-1] = value;
+    }
+
+    //ADD VALUES TO THE OPERATION
+    addOperation(value){
+        if(isNaN(this.getLastOperation())){
+            if(isNaN(value)){
+                this.setLastOperation(value);
+            } else {
+                this.pushOperation(value);
+                this.setLastNumberOnDisplay();
+            }
+        } else if(this.isOperator(value)){
+            this.pushOperation(value);
+        } else {
+            let newValue = this.getLastOperation().toString()+value.toString();
+            this.setLastOperation(parseInt(newValue));
+            this.setLastNumberOnDisplay();
+        }
+        console.log(this._operation);
+    }
+
+    //RETURNS LAST VALUE OF THE OPERATION
+    getLastOperation(){
+        return this._operation[this._operation.length-1];
+    }
+
+    //CALCULATES THE OPERATION
+    calc(){
+        let result;
+        if(this._operation.length > 3){
+            let last = this._operation.pop();
+            result = eval(this._operation.join(""));
+            this._operation = [result, last];
+        }
+
+        this.setLastNumberOnDisplay();
+    }
+
+    //SHOW NUMBER ON DISPLAY
+    setLastNumberOnDisplay(){
+        let last;
+
+        for(let i = this._operation.length-1; i >= 0; i--){
+            if(!this.isOperator(this._operation[i])){
+                last = this._operation[i];
+                console.log(last);
+                break;
+            }
+        }
+
+        this.displayCalc = last;
+    }
 
     //GETS AND SETTERS
     get displayCalc(){
